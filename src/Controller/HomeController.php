7f6af01478c8ab3c59c0ac59cfab739a25d7c612
Pavel <fandopaveel@gmail.com>
2019-01-19 23:04:34 +0300

    add RegistrationFormType and UserType

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,8 @@
 
 namespace App\Controller;
 
-
+use App\Entity\Post;
+use App\Entity\User;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PostRepository")
+ * @method getOwner()
  */
 class Post
 {
